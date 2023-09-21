e86b8f27f517998af853f91ad95c592fc634416e
J-odion <emmyjaff22@gmail.com>
2023-09-21 21:44:09 +0100

    Initial Commit

---
+++ weathr/weathrApp.swift
@@ -0,0 +1,17 @@
+//
+//  weathrApp.swift
+//  weathr
+//
+//  Created by MacBook on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct weathrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
